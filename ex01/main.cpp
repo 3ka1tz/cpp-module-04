01fdf3aeac73f1b274a23a261ca1e3dbb47e225f
3ka1tz <ekaitzlopez2025@gmail.com>
2025-11-14 12:18:09 +0100

    prepared for evaluation

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 
+#include "Animal.hpp"
 #include "Cat.hpp"
+#include "Dog.hpp"
 
 int main() {
+    const int size = 10;
+    Animal* animals[size];
+
+    for (int i = 0; i < size / 2; i++) {
+        animals[i] = new Cat();
+    }
+
+    for (int i = size / 2; i < size; i++) {
+        animals[i] = new Dog();
+    }
+
+    for (int i = 0; i < size; i++) {
+        animals[i]->makeSound();
+    }
+
     Cat originalCat;
     originalCat.setIdea(0, "Old idea0");
     originalCat.setIdea(1, "Old idea1");
 
-    Cat copiedCat = originalCat;
-    copiedCat.setIdea(0, "New idea0");
+    Cat copiedCatA(originalCat);
+    copiedCatA.setIdea(0, "New idea0");
+
+    Cat copiedCatB;
+    copiedCatB = originalCat;
+    copiedCatB.setIdea(1, "New idea1");
 
     std::cout << "originalCat Idea[0]: " << originalCat.getIdea(0) << std::endl;
     std::cout << "originalCat Idea[1]: " << originalCat.getIdea(1) << std::endl;
-    std::cout << "copiedCat   Idea[0]: " << copiedCat.getIdea(0) << std::endl;
-    std::cout << "copiedCat   Idea[1]: " << copiedCat.getIdea(1) << std::endl;
+    std::cout << "copiedCatA  Idea[0]: " << copiedCatA.getIdea(0) << std::endl;
+    std::cout << "copiedCatA  Idea[1]: " << copiedCatA.getIdea(1) << std::endl;
+    std::cout << "copiedCatB  Idea[0]: " << copiedCatB.getIdea(0) << std::endl;
+    std::cout << "copiedCatB  Idea[1]: " << copiedCatB.getIdea(1) << std::endl;
+
+    for (int i = 0; i < size; i++) {
+        delete animals[i];
+    }
 
     return 0;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,18 +1,49 @@
+#include <iostream>
+
 #include "AAnimal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
 
 int main() {
     // const AAnimal* animal = new AAnimal();
-    const AAnimal* cat = new Cat();
-    const AAnimal* dog = new Dog();
-
     // animal->makeSound();
-    cat->makeSound();
-    dog->makeSound();
 
-    delete cat;
-    delete dog;
+    const int size = 10;
+    AAnimal* animals[size];
+
+    for (int i = 0; i < size / 2; i++) {
+        animals[i] = new Cat();
+    }
+
+    for (int i = size / 2; i < size; i++) {
+        animals[i] = new Dog();
+    }
+
+    for (int i = 0; i < size; i++) {
+        animals[i]->makeSound();
+    }
+
+    Cat originalCat;
+    originalCat.setIdea(0, "Old idea0");
+    originalCat.setIdea(1, "Old idea1");
+
+    Cat copiedCatA(originalCat);
+    copiedCatA.setIdea(0, "New idea0");
+
+    Cat copiedCatB;
+    copiedCatB = originalCat;
+    copiedCatB.setIdea(1, "New idea1");
+
+    std::cout << "originalCat Idea[0]: " << originalCat.getIdea(0) << std::endl;
+    std::cout << "originalCat Idea[1]: " << originalCat.getIdea(1) << std::endl;
+    std::cout << "copiedCatA  Idea[0]: " << copiedCatA.getIdea(0) << std::endl;
+    std::cout << "copiedCatA  Idea[1]: " << copiedCatA.getIdea(1) << std::endl;
+    std::cout << "copiedCatB  Idea[0]: " << copiedCatB.getIdea(0) << std::endl;
+    std::cout << "copiedCatB  Idea[1]: " << copiedCatB.getIdea(1) << std::endl;
+
+    for (int i = 0; i < size; i++) {
+        delete animals[i];
+    }
 
     return 0;
 }
